9cfb74d9383b645a3551db5fd383d38ac1f0def2
Tanzim Ahmed <109636151+tanzim2000@users.noreply.github.com>
2022-07-26 08:53:57 -0400

    Delete Lab 03/doc directory

--- Lab 03/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"studentlab","l":"Bicycle"},{"p":"studentlab","l":"Car"},{"p":"studentlab","l":"TestDemo"}];updateSearchResults();
